e80874ffaec48c34d7b0a16d800eee4b00968309
Akshat Tripathi <at619@ic.ac.uk>
2020-01-10 11:55:36 +0000

    botimus prime

--- bot1.py
+++ bot1.py
@@ -12,14 +12,19 @@ from pause import until, datetime, sleep
 import timeit
 
 
-# TICKET_ID = "89040641957"
-TICKET_ID = "89217230137"
+# TICKET_ID = "-89040641957" # HACKHACK
+# TICKET_ID = "-89217230137" # HACK1040
+# TICKET_ID = "-89221236119" #TEST
+TICKET_ID = "-81611641617"
+
 X, Y = 600, 270
 DELTA_Y = 100
-# URL = "https://www.eventbrite.co.uk/e/hackhack-tickets-"
-URL = "https://www.eventbrite.co.uk/e/hack1040-tickets-"
+# URL = "https://www.eventbrite.co.uk/e/hackhack-tickets"
+# URL = "https://www.eventbrite.co.uk/e/hack1040-tickets"
+# URL = "https://www.eventbrite.co.uk/e/test-tickets"
+URL = "https://www.eventbrite.co.uk/e/ic-hack-20-tickets"
 # LAUNCH = datetime(2020, 1, 10, 13, 30, 0, 100000)
-LAUNCH = datetime(2020, 1, 10, 11, 1, 46, 100000)
+LAUNCH = datetime(2020, 1, 10, 13, 29, 59, 100000)
 
 REG_X, REG_Y = 600, 750
 
@@ -44,4 +49,4 @@ move(REG_X, REG_Y)
 click()
 e = timeit.default_timer()
 print(load - s)
-print(e - s)
+print(e - load)
